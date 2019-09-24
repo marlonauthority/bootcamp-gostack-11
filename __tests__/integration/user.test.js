2d00840d7d241c01a6dbd66562356e6b96f40e48
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-23 20:02:29 -0400

    test email duplicado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +17,24 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be ale to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Marlon',
+        email: 'fake@email.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Marlon',
+        email: 'fake@email.com',
+        password_hash: '123456',
+      });
+    expect(response.status).toBe(400);
   });
 });
